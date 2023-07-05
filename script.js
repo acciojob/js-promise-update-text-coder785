061c9b2077c848fe24b1bf3b00cfa825b1b54d53
coder785 <116183840+coder785@users.noreply.github.com>
2023-07-05 22:08:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let p=document.createElement("p");
+p.setAttribute('id','timer')
+document.body.append(p);
+setInterval(function(){
+ let date=new Date();
+ let an=date.getMonth()+"/"+date.getDate()+"/"+date.getFullYear()+", "+date.getHours()+":"+date.getMinutes()+":"+date.getSeconds();
+// console.log(typeof date.getHours());
+ if(date.getHours()>=12 && date.getHours()<=23)
+ {
+    an=an+" PM";
+ }
+ else{
+an=an+" AM"
+ }
+ p.innerHTML=an;
+},1000);
